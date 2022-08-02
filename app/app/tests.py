0a1f34b0802284b6371beb250d72e04dd0a743ac
bryanbuitrago <bryanbuitrago86@gmail.com>
2022-08-02 15:53:24 -0400

    Fix syntax error

--- app/app/tests.py
+++ app/app/tests.py
@@ -12,8 +12,6 @@ class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
         """Test adding numbers together."""
-
         res = calc.add(5, 6)
 
-
-self.assertEqual(res, 11)
+        self.assertEqual(res, 11)
