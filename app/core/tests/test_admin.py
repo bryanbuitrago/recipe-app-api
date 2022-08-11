0118f37f001e71122975d8eeac146ce743aed3f0
bryanbuitrago <bryanbuitrago86@gmail.com>
2022-08-11 17:21:01 -0400

    Make syntax correction.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,4 +1,6 @@
-"""Tests for the django admin modifications."""
+"""
+Tests for the Django admin modifications.
+"""
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
