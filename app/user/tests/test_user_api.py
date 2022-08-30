77a19eb6cf2b3245364e98a050156ef5159420b6
bryanbuitrago <bryanbuitrago86@gmail.com>
2022-08-29 22:02:37 -0400

    Remove unneeded modules.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -1,8 +1,6 @@
 """
 Test for the user API. 
 """
-
-from secrets import token_urlsafe
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
