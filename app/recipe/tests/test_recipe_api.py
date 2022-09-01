cb6e1fb4efcf861711a5f78a90738214ed06230b
bryanbuitrago <bryanbuitrago86@gmail.com>
2022-08-31 21:18:23 -0400

    Uncomment test.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -172,15 +172,15 @@ class PrivateRecipeApiTests(TestCase):
         recipe.refresh_from_db()
         self.assertEqual(recipe.user, self.user)
 
-    # def test_delete_recipe(self):
-    #     """Test deleting a recipe sucessful."""
-    #     recipe = create_recipe(user=self.user)
+    def test_delete_recipe(self):
+        """Test deleting a recipe sucessful."""
+        recipe = create_recipe(user=self.user)
 
-    #     url = detail_url(recipe.id)
-    #     res = self.clinet.delete(url)
+        url = detail_url(recipe.id)
+        res = self.client.delete(url)
 
-    #     self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
-    #     self.assertFalse(Recipe.objects.filter(id=recipe.id).exists())
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Recipe.objects.filter(id=recipe.id).exists())
 
     # def test_recipe_other_users_recipe_error(self):
     #     """Test trying to delete another user's recipe gives error."""
