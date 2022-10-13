3d94f5ac9fc0cdef6c44ab2d8d8af4f4dcbcb522
bryanbuitrago <bryanbuitrago86@gmail.com>
2022-10-13 19:49:09 -0400

    Typos fix.

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@
 set -e
 
 python3 manage.py wait_for_db
-python3 manage.py collesctstatic --nopinput
+python3 manage.py collectstatic --noinput
 python3 manage.py migrate
 
-uswgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
+uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
